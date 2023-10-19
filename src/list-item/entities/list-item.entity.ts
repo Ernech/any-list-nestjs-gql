565fa6e9c3745c5f9718149162c41b453161f6e0
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2023-10-19 18:06:18 -0400

    Create list item

--- src/list-item/entities/list-item.entity.ts
+++ src/list-item/entities/list-item.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { Item } from 'src/items/entities/item.entity';
 import { List } from 'src/list/entities/list.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
@@ -8,12 +8,15 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 export class ListItem {
 
   @PrimaryGeneratedColumn('uuid')
+  @Field(()=>ID)
   id:string;
 
   @Column({type:'numeric'})
+  @Field(()=>Number)
   quantity:number;
 
   @Column({type:'boolean'})
+  @Field(()=>Boolean)
   completed:boolean
 
   @ManyToOne(()=>List, (list)=>list.listItem,{lazy:true})  
@@ -24,3 +27,5 @@ export class ListItem {
   item:Item;
 
 }
+
+//@Unique('listItem-item', ['list','item'])
--- src/list-item/list-item.resolver.ts
+++ src/list-item/list-item.resolver.ts
@@ -3,13 +3,19 @@ import { ListItemService } from './list-item.service';
 import { ListItem } from './entities/list-item.entity';
 import { CreateListItemInput } from './dto/create-list-item.input';
 import { UpdateListItemInput } from './dto/update-list-item.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @Resolver(() => ListItem)
+@UseGuards(JwtAuthGuard)
 export class ListItemResolver {
   constructor(private readonly listItemService: ListItemService) {}
 
   @Mutation(() => ListItem)
-  createListItem(@Args('createListItemInput') createListItemInput: CreateListItemInput) {
+  createListItem(
+    @Args('createListItemInput') createListItemInput: CreateListItemInput
+    //! TODO: Verify if the list belongs to the user
+    ) {
     return this.listItemService.create(createListItemInput);
   }
 
@@ -18,18 +24,18 @@ export class ListItemResolver {
     return this.listItemService.findAll();
   }
 
-  @Query(() => ListItem, { name: 'listItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.findOne(id);
-  }
+  // @Query(() => ListItem, { name: 'listItem' })
+  // findOne(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.findOne(id);
+  // }
 
-  @Mutation(() => ListItem)
-  updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
-    return this.listItemService.update(updateListItemInput.id, updateListItemInput);
-  }
+  // @Mutation(() => ListItem)
+  // updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
+  //   return this.listItemService.update(updateListItemInput.id, updateListItemInput);
+  // }
 
-  @Mutation(() => ListItem)
-  removeListItem(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.remove(id);
-  }
+  // @Mutation(() => ListItem)
+  // removeListItem(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.remove(id);
+  // }
 }
